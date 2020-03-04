ce61fe27283201ef8b1d232b5749cba1e39fb9ce
irebai <irebai@linagora.com>
2020-03-04 14:53:55 +0100

    Update test_deployment.sh

--- test/test_deployment.sh
+++ test/test_deployment.sh
@@ -1,1 +1,1 @@
-curl -X POST "http://localhost:8888/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "wavFile=@bonjour.wav;type=audio/wav"
+curl -X POST "http://localhost:8888/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@bonjour.wav;type=audio/wav"
