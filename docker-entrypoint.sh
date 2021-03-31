53116ee7f7428e41a6736c245028a0cd2c8a43a1
Ilyes Rebai <irebai@linagora.com>
2021-03-31 16:12:27 +0200

    fix text punctuation error and clean entrypoint code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,25 +8,15 @@ for retry in $(seq 1 $max_attempts); do
     echo "Waiting punctuation service... [attempt=$retry]"
     punctuation_state=1
     ./wait-for-it.sh $PUCTUATION_HOST:$PUCTUATION_PORT --timeout=$delay || punctuation_state=0
+    if [ $punctuation_state == 1 ]; then break; fi
 done
-
-if [ $punctuation_state == 1 ]; then
-    echo "$PUCTUATION_HOST:$PUCTUATION_PORT is up"
-else
-    echo "punctuation service is not runninig"
-fi
 
 for retry in $(seq 1 $max_attempts); do
     echo "Waiting speaker diarization service... [attempt=$retry]"
     spkdiarization_state=1
     ./wait-for-it.sh $SPEAKER_DIARIZATION_HOST:$SPEAKER_DIARIZATION_PORT --timeout=$delay || spkdiarization_state=0
+    if [ $spkdiarization_state == 1 ]; then break; fi
 done
-
-if [ $spkdiarization_state == 1 ]; then
-    echo "$SPEAKER_DIARIZATION_HOST:$SPEAKER_DIARIZATION_PORT is up"
-else
-    echo "speaker diarization service is not runninig"
-fi
 
 echo "RUNNING service"
 
