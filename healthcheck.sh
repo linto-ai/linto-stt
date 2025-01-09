ccfc95159949a562eed0613336e78c89d677145d
AudranBert <bert.audran@gmail.com>
2025-01-09 14:28:15 +0100

    change websocket healthcheck + add netcat in dockers
    Signed-off-by: AudranBert <bert.audran@gmail.com>

--- healthcheck.sh
+++ healthcheck.sh
@@ -13,8 +13,7 @@ else
     else
         if [ "$SERVICE_MODE" = "websocket" ]
         then
-            python3 websocket/healthcheck.py ${STREAMING_PORT:=80} || exit 1
-            exit 0
+            nc -z localhost ${STREAMING_PORT:=80} && exit 0 || exit 1
         else
             celery --app=celery_app.celeryapp inspect ping -d ${SERVICE_NAME}_worker@$HOSTNAME || exit 1
         fi
