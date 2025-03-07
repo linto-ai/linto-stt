beeefc23ef0bf81361787db8c4e293410d83cab1
AudranBert <bert.audran@gmail.com>
2025-03-07 16:38:07 +0100

    improve language retrieval

--- nemo/stt/processing/utils.py
+++ nemo/stt/processing/utils.py
@@ -32,7 +32,14 @@ def get_language(language = None):
     if language is None:
         language = os.environ.get("LANGUAGE", None)
     if language is None:
-        language = "fr" if "fr" in os.environ.get("MODEL", "nvidia/stt_fr_conformer_ctc_large").split("_") else "en"
+        model = os.environ.get("MODEL", "nvidia/stt_fr_conformer_ctc_large").split("_")
+        languages = ["fr", "en", "it", "ru", "ar", "es", "de"]
+        for l in languages:
+            if l in model:
+                language = l
+                break
+        if language is None:
+            language = "en"
     return language
 
 def get_model_class(architecture):
