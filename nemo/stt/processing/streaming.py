d8533b94a1f19a510333e6bbce7ff2b62ea82e1b
AudranBert <bert.audran@gmail.com>
2025-06-05 11:12:54 +0200

    re handling transcription errors

--- nemo/stt/processing/streaming.py
+++ nemo/stt/processing/streaming.py
@@ -225,11 +225,11 @@ class StreamingASRProcessor:
                 logger.error(f"Encoutered an error while transcribing: {e}")
                 return (None, None, ""), self.to_flush(self.buffered_final.copy())
         else:
-            # try:
-            hypothesis = self.model.transcribe([self.audio_buffer], return_hypotheses=True, timestamps=True, verbose=False)[0]
-            # except ValueError as e:
-            #     logger.error(f"Encoutered an error while transcribing: {e}")
-            # return (None, None, ""), self.to_flush(self.buffered_final.copy())
+            try:
+                hypothesis = self.model.transcribe([self.audio_buffer], return_hypotheses=True, timestamps=True, verbose=False)[0]
+            except ValueError as e:
+                logger.error(f"Encoutered an error while transcribing: {e}")
+            return (None, None, ""), self.to_flush(self.buffered_final.copy())
         if isinstance(hypothesis, list):
             hypothesis = hypothesis[0]
         formatted_words = self.format_words(hypothesis.timestamp['word'], convertion_function if self.vad else None)
