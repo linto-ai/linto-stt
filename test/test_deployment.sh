f2f6d72e6485c15d8a8ffd118576eb5f8a0d8a8f
Audran Bert <bert.audran@gmail.com>
2025-06-26 17:50:34 +0200

    adjust variables

--- test/test_deployment.sh
+++ test/test_deployment.sh
@@ -1,3 +1,7 @@
 audio=${1:-bonjour.wav}
-ffmpeg -y -i $audio -acodec pcm_s16le -ar 16000 -ac 1 tmp.wav
-curl -X POST "http://localhost:8080/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@tmp.wav;type=audio/wav"
+if audio endswith .mp3; then
+    ffmpeg -y -i $audio -acodec pcm_s16le -ar 16000 -ac 1 tmp.wav
+    curl -X POST "http://localhost:8080/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@tmp.wav;type=audio/wav"
+else
+    curl -X POST "http://localhost:8080/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@$audio;type=audio/wav"
+fi
