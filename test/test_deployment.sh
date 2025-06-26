5425b4435671a65ef18e2e8dc88c49d2682ebb7d
tjiho <tom.darboux@laposte.net>
2025-06-26 17:59:42 +0200

    fix test script condition when sending non wav file

--- test/test_deployment.sh
+++ test/test_deployment.sh
@@ -1,7 +1,8 @@
 audio=${1:-bonjour.wav}
-if audio endswith .mp3; then
+if [[ $audio == *.wav ]]
+then
+    curl -X POST "http://localhost:8080/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@$audio;type=audio/wav"
+else
     ffmpeg -y -i $audio -acodec pcm_s16le -ar 16000 -ac 1 tmp.wav
     curl -X POST "http://localhost:8080/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@tmp.wav;type=audio/wav"
-else
-    curl -X POST "http://localhost:8080/transcribe" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@$audio;type=audio/wav"
 fi
