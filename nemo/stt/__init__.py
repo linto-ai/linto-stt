d3d1e82e0f2f058b87ebe9cbb1d0c4f20a8db165
Audran Bert <bert.audran@gmail.com>
2025-07-04 15:27:57 +0200

    clean code

--- nemo/stt/__init__.py
+++ nemo/stt/__init__.py
@@ -28,7 +28,7 @@ VAD_MIN_SILENCE_DURATION = float(os.environ.get("VAD_MAX_SILENCE_DURATION", 0.1)
 
 STREAMING_MIN_CHUNK_SIZE=float(os.environ.get("STREAMING_MIN_CHUNK_SIZE", 0.5))
 STREAMING_BUFFER_TRIMMING_SEC=float(os.environ.get("STREAMING_BUFFER_TRIMMING_SEC", 10.0))
-STREAMING_PAUSE_FOR_FINAL=float(os.environ.get("STREAMING_PAUSE_FOR_FINAL", 2.0))
+STREAMING_PAUSE_FOR_FINAL=float(os.environ.get("STREAMING_PAUSE_FOR_FINAL", 1.2))
 STREAMING_TIMEOUT_FOR_SILENCE=os.environ.get("STREAMING_TIMEOUT_FOR_SILENCE", None)  # will consider that silence is detected if no audio is received for the duration of the paquet (dtermined from the first message) * this variable
 if STREAMING_TIMEOUT_FOR_SILENCE:
     STREAMING_TIMEOUT_FOR_SILENCE = float(STREAMING_TIMEOUT_FOR_SILENCE)
