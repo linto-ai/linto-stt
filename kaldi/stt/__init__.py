4d6b51c07720e9d9b6ac01914a546cd1bfb0194a
tjiho <tom.darboux@laposte.net>
2025-11-04 18:00:46 +0100

    Setup nemo http

--- kaldi/stt/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-import os
-
-logging.basicConfig(
-    format="%(asctime)s %(name)s %(levelname)s: %(message)s",
-    datefmt="%d/%m/%Y %H:%M:%S",
-)
-logger = logging.getLogger("__stt__")
