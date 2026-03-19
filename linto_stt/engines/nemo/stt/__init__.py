19e0935639d55924cf5e124a95656f0333f2f240
Jeronymous <jeromelouradour@gmail.com>
2026-03-19 16:55:53 +0100

    Do not enable VAD by default

--- linto_stt/engines/nemo/stt/__init__.py
+++ linto_stt/engines/nemo/stt/__init__.py
@@ -14,13 +14,13 @@ logger = logging.getLogger("__stt__")
 # see https://github.com/guillaumekln/faster-whisper/issues/150
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # GPU in the right order
 
-vad = os.environ.get("VAD","auditok").lower()
+vad = os.environ.get("VAD","false").lower()
 if vad in ["true", "1"]:
     VAD = "auditok"
 elif vad in ["false", "0"]:
     VAD = False
 else:
-    VAD = os.environ.get("VAD","auditok")
+    VAD = vad
 
 VAD_DILATATION = float(os.environ.get("VAD_DILATATION", 0.5))
 VAD_MIN_SPEECH_DURATION = float(os.environ.get("VAD_MIN_SPEECH_DURATION", 0.1))
